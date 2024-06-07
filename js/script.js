13e5ed09e0d908d6f659e19157531b0fa5f27596
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-06-07 16:31:58 -0300

    sessao 4 finalizada e estilizada

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ function alternarMenu(){
     nav.classList.toggle('aberto')
 }
 // Fim controle do menu
+
+// Sessão 4 - Público-Alvo
+function checarResposta() {
+    const resposta = document.querySelector('input[name="resposta"]:checked');
+    const explicacao = document.getElementById('explicacao');
+  
+    if (resposta && resposta.value === "correto") {
+      explicacao.textContent = "Está correto! A vida marinha é preservada e os nós, por consequência, consumimos os alimentos de maneira limpa e nutritiva, sem resíduos plásticos.";
+      explicacao.style.color = "#0a7e91";
+    } else {
+      explicacao.textContent = "Incorreto. Tente novamente.";
+      explicacao.style.color = "red";
+    }
+  }
+  
