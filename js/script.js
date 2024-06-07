15c25f0aa5d78f5e39e5f4afd371c7fbd54141e7
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-06-07 19:47:35 -0300

    sessao 6 finalizada, estilizada e responsividade adicionada

--- js/script.js
+++ js/script.js
@@ -47,8 +47,15 @@ function ajustarResponsividade() {
 }
 
 ajustarResponsividade();
-window.addEventListener('resize', ajustarResponsividade);
+window.addEventListener('resize', ajustarResponsividade());
 
 let intervaloCarrossel = setInterval(carrossel, tempoIntervalo);
 // Fim Sessão 5 - Solução 
-  
+
+// Sessão 6 - Cotidiano
+function mostrarMsg() {
+  const mensagemContainer = document.getElementById('msg');
+  mensagemContainer.textContent = "O software te ajuda no dia a dia! Se na sua dieta alimentos provenientes do mar estão presentes, então essa solução é de extrema importância para sua vida, pois evita que os animais marítimos se alimentem de plásticos. Nós, então, não acabamos por consumir micro-plásticos ou outros resíduos que poluem nossa alimentação.";
+}
+
+// Fim Sessão 6 - Cotidiano
